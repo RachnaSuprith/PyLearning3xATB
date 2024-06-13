e68384636f140b23ecc542781d1b9403e4135908
RachnaSuprith <rachna.suprith@gmail.com>
2024-06-13 12:38:57 +0530

    Variables and Data Types

--- Ex_05062024/Lab008.py
+++ Ex_05062024/Lab008.py
@@ -1,6 +1,4 @@
-#Take an input from username and print
-#Name=input("Enter your name")
-#print ("your name is",Name)
-first_name=input("enter your first name")
-last_name=input("enter your last name")
-print ("your first name is",first_name, "and your last name is",last_name)
+print ("hello world")
+
+
+
