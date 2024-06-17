07992930bc7ae2efd291e8e8321d16ae92d68ba3
RachnaSuprith <rachna.suprith@gmail.com>
2024-06-17 13:30:29 +0530

    Variables and Data Types

--- Ex_07062024/Labtask3.py
+++ Ex_07062024/Labtask3.py
@@ -1,23 +1,31 @@
-''#Develop a Python script that calculates the square and cube of a given number. num = 2 sq - 4, c = 8
-
-# Take User Input
-Number = int(input(print("Enter the number")))
-Square = Number ** 2
-Cube = Number ** 3
-print(f"Square of {Number} is {Square}")
-print(f"Cube of {Number} is {Cube}")
-
-
-
-
-#Create a program that takes two numbers as input and prints whether the first number is greater than, less than, or equal to the second number.
-
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+# compare numbers (num1,num2)
+num1 = float(input("enter the first number:"))
+num2 = float(input("enter the second number:"))
 
 if num1 > num2:
-    print(f"{num1} is greater than {num2}")
+     print(f"{num1} is greater than {num2}")
 elif num1 < num2:
-    print(f"{num1} is less than {num2}")
+     print(f"{num2} is greater than {num1}")
 else:
-    print(f"{num1} is equal to {num2}")
+    #print(f"{num1} is equal to {num2}")
+
+
+
+#num1 = int(input("enter first num: "))
+#num2 = int(input("enter second num: "))
+
+#if num1 > num2:
+    #print(f"first num = {num1} is greater")
+#elif num1 < num2:
+    print(f"second num = {num2} is greater")
+#elif num1 == num2:
+    print("both nums are equal")
+#elif num1 < num2:
+    #print(f"first num = {num1} is less")
+#else:
+    #print(f"second num = {num2} is less",)
+
+
+#Num1 = float(input("Enter the first number\n"))
+#Num2 = float(input("Enter the second number\n"))
+#print("First number is greater or equals to second"if Num1 >= Num2 else"First number is Less than second number")
